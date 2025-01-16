29cc319d1799e3b5bbc3a6f84cf4b1c6c7c66e82
Ales Brelih <ales.brelih@3fs.si>
2025-01-16 19:51:37 +0100

    fix(position type): fixed matching for project file includes
    Now supports single line arrays as well.
    For example: file: ["/file", "/second"]

--- src/gitlab_ci_ls_parser/treesitter_queries.rs
+++ src/gitlab_ci_ls_parser/treesitter_queries.rs
@@ -488,7 +488,7 @@ impl TreesitterQueries {
                                                             block_mapping(
                                                             block_mapping_pair
                                                                 key: (flow_node(plain_scalar(string_scalar)@file_key))
-                                                                value: [(block_node(block_sequence(block_sequence_item(flow_node)@file_value)))(flow_node)@file_value]
+                                                                value: [(block_node(block_sequence(block_sequence_item(flow_node)@file_value)))(flow_node(flow_sequence(flow_node)@file_value))(flow_node([(double_quote_scalar)(single_quote_scalar)(plain_scalar)]))@file_value]
                                                             )
                                                         )
                                                     ]
